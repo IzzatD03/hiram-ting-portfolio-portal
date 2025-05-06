e3b76fbaf6cd256cc43900903d2a38187d32859c
IzzatD03 <143278422+IzzatD03@users.noreply.github.com>
2025-05-06 11:21:59 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    // ✅ Add this line to support React Router fallback
+    historyApiFallback: true,
   },
   plugins: [
     react(),
